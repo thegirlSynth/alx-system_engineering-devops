fc8698b8df7a460a0dcebd22e37100d7615996d6
Cynthia Uche <cynthianuchepro@gmail.com>
2023-04-07 13:29:13 -0700

    Create 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,6 @@
 # Sets up the client SSH config file so that you can connect to a server without a password
 
 exec { 'configure_ssh':
-  command => 'echo -e "\n  IdentityFile ~/.ssh/school\n  PasswordAuthentication no\n" >> /etc/ssh/config',
+  command => 'echo "\n  IdentityFile ~/.ssh/school\n  PasswordAuthentication no\n" >> /etc/ssh/ssh_config',
   path => '/usr/bin',
-  unless  => 'grep -q "IdentityFile ~/.ssh/school" ~/.ssh/config && grep -q "PasswordAuthentication no" /etc/ssh/config',
 }
