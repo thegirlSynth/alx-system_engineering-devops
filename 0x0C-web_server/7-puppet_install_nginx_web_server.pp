2f2e68050395b132ce29c6142f02a6f396502485
Cynthia Uche <cynthianuchepro@gmail.com>
2023-04-14 06:47:48 -0700

    Insert omitted backslash

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,5 +2,5 @@
 
 exec {'Nginx Installation':
   provider => shell,
-  command => 'apt-get update ; apt-get -y install nginx ; echo "Hello World!" > /var/www/html/index.html ; sed -i "s/server_name _;/server_name _;\n\tlocation \/redirect_me {\n\treturn 301 https:\/\/dev.to\/thegirlsynth;\n\t}" /etc/nginx/sites-available/default ; service nginx restart',
+  command => 'apt-get update ; apt-get -y install nginx ; echo "Hello World!" > /var/www/html/index.html ; sed -i "s/server_name _;/server_name _;\n\tlocation \/redirect_me {\n\treturn 301 https:\/\/dev.to\/thegirlsynth;\n\t}/" /etc/nginx/sites-available/default ; service nginx restart',
 }
