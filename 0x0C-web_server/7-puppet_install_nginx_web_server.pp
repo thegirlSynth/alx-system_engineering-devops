c97287639f0f7cee4d6defc29db5896da2e0bcf7
Cynthia Uche <cynthianuchepro@gmail.com>
2023-04-14 05:53:50 -0700

    Fix Nginx install errors

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,29 +1,6 @@
-#Installing and configuring Nginx 
-class { 'nginx': }
+#Installs Nginx web server
 
-# Creating index page
-file { '/var/www/html/index.html':
-  ensure => present,
-  content => 'Hello World!',
-}
-
-# Creating custom 404 page
-file { '/var/www/html/404.html':
-  ensure => present,
-  content => 'Ceci n\'est pas une page',
-}
-
-# Redirecting /redirect_me
-file_line { 'nginx-config-line':
-  path	=> '/etc/nginx/sites-available/default',
-  line	=> '/server_name _;', 
-  match	=> '^server_name _;$',
-  replace => 'server_name _;\n\terror_page 404 \/404.html;\n\tlocation \/redirect_me {\n\treturn 301 https:\/\/dev.to\/thegirlsynth;\n\t}',
-}
-
-# Restarting Nginx
-service { 'nginx':
-  ensure => running,
-  enable => true,
-  require => File_line['nginx-config-line'],
+exec {'Nginx Installation':
+  provider => shell,
+  command => 'apt-get update ; apt-get -y install nginx ; echo "Hello World!" > /var/www/html/index.html ; sed -i "s/server_name _;/server_name _;\n\tlocation \/redirect_me {\n\treturn 301 https:\/\/dev.to\/thegirlsynth;\n\t}" /etc/nginx/sites-available/default ; service nginx restart',
 }
