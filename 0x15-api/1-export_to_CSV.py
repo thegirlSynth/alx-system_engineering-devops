cf0c17c257d4cdd6d9b6e8edb2f04c7fa5e13ff2
Cynthia Uche <cynthianuchepro@gmail.com>
2023-05-22 15:08:53 -0700

    Create 1-export_to_CSV.py

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""This module exports data gathered from an api in csv format"""
+
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    id = sys.argv[1]
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
+    response = requests.get(url)
+    employee_name = response.json().get('username')
+
+    task_response = requests.get(url + "/todos")
+    tasks = task_response.json()
+
+    csv_data = []
+    for todo in tasks:
+        csv_data.append([id, employee_name, todo['completed'], todo['title']])
+
+    with open('{}.csv'.format(id), 'w', newline="") as csv_file:
+        writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+        writer.writerows(csv_data)
