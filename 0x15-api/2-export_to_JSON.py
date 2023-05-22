8a6750bc23ce60b881d30bf85c9fd6035d708fd2
Cynthia Uche <cynthianuchepro@gmail.com>
2023-05-22 15:09:20 -0700

    Create 2-export_to_JSON.py

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This module gathers data from an api"""
+
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    id = sys.argv[1]
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
+    response = requests.get(url)
+    employee_name = response.json().get('username')
+
+    task_response = requests.get(url + "/todos")
+    tasks = task_response.json()
+
+    json_data = {str(id): []}
+    for task in tasks:
+        task_data = {
+            'task': task['title'],
+            'completed': task['completed'],
+            'username': employee_name
+        }
+        json_data[str(id)].append(task_data)
+
+    with open('{}.json'.format(id), 'w') as file:
+        json.dump(json_data, file)
