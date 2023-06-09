7ec0246beecd2280749b4cdf55029dacb2b38de2
The Cynthia Uche <cynthianuchepro@gmail.com>
2023-06-09 04:34:31 +0100

    Create 1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module ccontains a function `top_ten`"""
+
+import json
+import requests
+
+def top_ten(subreddit):
+    """Queries the Reddit API and prints the titles of the first 10 hot posts"""
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "NewScript/1.0"}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        return None
+
+    posts = response.json().get("data").get("children")
+
+    for post in posts[:10]:
+        print(post.get("data").get("title"))
